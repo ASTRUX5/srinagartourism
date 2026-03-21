f2ee63ff8aa8807711ccd42700d0df20a2e140f5
ASTRUX5 <zfdwqe9534@gmail.com>
2026-03-21 13:54:37 +0530

    Update images.js

--- images.js
+++ images.js
@@ -36,7 +36,7 @@ var IMAGES = {
   card_srinagar:           'https://images.pexels.com/photos/7743323/pexels-photo-7743323.jpeg',
   card_pahalgam:           'https://images.pexels.com/photos/4904290/pexels-photo-4904290.jpeg?w=1400',
   card_sonamarg_gulmarg:   'https://images.pexels.com/photos/16526498/pexels-photo-16526498.jpeg?w=1400⁠',
-  card_kashmir_special:    'https://images.unsplash.com/photo-1506905925346-21bda4d32df4?w=600&q=80&auto=format&fit=crop',
+  card_kashmir_special:    'https://images.pexels.com/photos/2439787/pexels-photo-2439787.jpeg?w=1400⁠',
   card_kashmir_holiday:    'https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5?w=600&q=80&auto=format&fit=crop',
   card_kashmir_pkg_tour:   'https://images.unsplash.com/photo-1464822759023-fed622ff2c3b?w=600&q=80&auto=format&fit=crop',
   card_honeymoon:          'https://images.unsplash.com/photo-1548013146-72479768bada?w=600&q=80&auto=format&fit=crop',
